3b2d67abc8214446f208932487fe8cadf766ccff
fairus0305 <ruszezzy@gmail.com>
2026-09-22 10:39:34 +0700

    membuat kode program python yang bisa mengevaluasi program kerja

---
+++ no.1.py
@@ -0,0 +1,14 @@
+kinerja = input ('masukan kinerja mahasiswa : ')
+
+if int(kinerja) >= 90 :
+    print('kinerja sangat baik')
+elif int(kinerja) >= 80 :
+    print('kinerja sangat baik')
+elif int(kinerja) >= 70 :
+    print('kinerja bagus')
+elif int(kinerja) >= 60 :
+    print('kinerja rata rata')
+
+else:
+    print ('semangat :)' )    
+
